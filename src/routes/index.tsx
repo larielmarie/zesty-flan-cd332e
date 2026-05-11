e45e24bc30afc00a2dda79d4112cc731177e262c
wilsonlariel-coder <wilson.lariel@gmail.com>
2026-05-11 17:27:58 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -125,7 +125,7 @@ function NavBar() {
               color: 'var(--maroon-dark)',
             }}
           >
-            <span style={{ filter: 'hue-rotate(-25deg) saturate(1.8)' }}>📞</span> Call Us
+            <span>📞</span> Call Us
           </a>
           <a
             href={EMAIL_HREF}
